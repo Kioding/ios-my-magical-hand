da81de2c1ab6b914fbaf380c955b1f8db1c01ff3
kio <un19451024@gmail.com>
2021-08-19 16:47:54 +0900

    Feat: UI 화면 구현

--- MyMagicalHand/MyMagicalHand/ViewController.swift
+++ MyMagicalHand/MyMagicalHand/ViewController.swift
@@ -8,8 +8,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var backView: UIView!
+    @IBOutlet weak var canvasView: UIView!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var percentLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func pressedResultButton(_ sender: Any) {
+    }
+    
+    @IBAction func pressedEraserButton(_ sender: Any) {
+    }
+    
 }
